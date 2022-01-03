71924fa5e516e12216e12d51ca719b9e3baa8665
JureHudoklin <32479419+JureHudoklin@users.noreply.github.com>
2022-01-03 19:59:18 +0900

    Update camera_calibration_service.py

--- src/camera_calibration_service.py
+++ src/camera_calibration_service.py
@@ -4,10 +4,11 @@ import rospy
 import cv2
 import numpy as np
 from sensor_msgs.msg import Image, CameraInfo
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Pose, TransformStamped
 from simple_camera_calibration.srv import SimpleCameraCalibration, SimpleCameraCalibrationResponse
 import tf2_ros as tf2
+from tf import transformations
 import utils
 
 
@@ -22,12 +23,14 @@ class CameraCalibrationService(object):
         self.camera_calibration_service = rospy.Service('camera_calibration', SimpleCameraCalibration, self.camera_calibration_callback)
 
         # Subscribers
-        self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.img_cb)
-        self.camera_info_sub = rospy.Subscriber('/camera/rgb/camera_info', CameraInfo, self.info_cb)
+        self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.img_cb)
+        self.camera_info_sub = rospy.Subscriber('/camera/color/camera_info', CameraInfo, self.info_cb)
+
+        self.tf_broadcaster = tf2.StaticTransformBroadcaster()
 
     def img_cb(self, img):
         try:
-            self.color_msg = msg
+            self.color_msg = img
             self.color_img = self.bridge.imgmsg_to_cv2(self.color_msg,"bgr8")
         except CvBridgeError as e:
             print(e)
@@ -47,11 +50,12 @@ class CameraCalibrationService(object):
         self.D = np.array(msg.D) # Distortion matrix. 5 for IntelRealsense, 8 for AzureKinect
 
     def camera_calibration_callback(self, req):
-
+        
         response = SimpleCameraCalibrationResponse()
+        assert (isinstance(response, SimpleCameraCalibrationResponse))
         # Get the image
         image = self.color_img
-        image = cv2.undistort(image, self.K, self.D)
+        #image = cv2.undistort(image, self.K, self.D)
         gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
         # Make the hand base transfrom into a matrix
@@ -63,36 +67,67 @@ class CameraCalibrationService(object):
         # Find the cheesboard corners
         objp = np.zeros((1, self.pattern_size[0] * self.pattern_size[1], 3), np.float32)
         objp[0,:,:2] = np.mgrid[0:self.pattern_size[0], 0:self.pattern_size[1]].T.reshape(-1, 2)
+        x = np.linspace(50, -50, 6, endpoint=True) / 1000
+        y = np.linspace(-40, 40, 5, endpoint=True) / 1000
+        xv, yv = np.meshgrid(x, y, indexing='xy')
+        for i in range(objp.shape[1]):
+            x_ind = i // self.pattern_size[0]
+            y_ind = i % self.pattern_size[0]
+            objp[0,i,:] =np.array([xv[x_ind, y_ind], yv[x_ind, y_ind], 0])
 
-        retval, corners = cv2.findChessboardCorners(gray, self.pattern_size, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+        
+        #print(gray)
+        retval, corners = cv2.findChessboardCorners(gray, self.pattern_size, None) #cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE
         # Optimize the corners
         if retval == True:
             print("Found corners")
             criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
             corners_2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+            cv2.drawChessboardCorners(image, self.pattern_size, corners_2, retval)
+            # self.temp_pub = rospy.Publisher("/chesboard_img", Image, 2)
+            # out_img = Image()
+            # out_img = self.bridge.cv2_to_imgmsg(image, "bgr8")
+            # for i in range(100):
+            #     self.temp_pub.publish(out_img)
+            #     rospy.sleep(0.1)
+            #cv2.imshow('img', gray)
+            #cv2.waitKey(500)
+
             self.objpoints.append(objp)
             self.imgpoints.append(corners_2)
         else:
             print("Corners not found")
-            response.success = False
+            response.success.data = False
             return response
 
         # Calibrate the camera
-        retval, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, gray.shape[::-1], None, None)
-        hand_camera_tf = self.make_pose(rvecs[0], tvecs[0])
-        hand_camera_tf_mat = utils.pose_to_matrix(hand_camera_tf)
-        camera_hand_tf_mat = tf.transformations.inverse_matrix(hand_camera_tf_mat)
+        # retval, _, _, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, gray.shape, self.K, self.D)
+        flags = cv2.SOLVEPNP_ITERATIVE
+        # solve pnp
+        (retval, rvecs, tvecs) = cv2.solvePnP(
+            self.objpoints[0],
+            self.imgpoints[0],
+            self.K,
+            self.D,
+            flags=flags)
 
-        camera_base_tf_mat = np.dot(camera_hand_tf_mat, hand_base_tf_mat)
+        hand_camera_tf = self.make_pose(rvecs, tvecs)
+        hand_camera_tf_mat = utils.pose_to_matrix(hand_camera_tf)
+        camera_hand_tf_mat = transformations.inverse_matrix(hand_camera_tf_mat)
+
+        camera_base_tf_mat = np.dot(hand_base_tf_mat, camera_hand_tf_mat)
         rospy.logwarn("Camera base tf: {}".format(camera_base_tf_mat))
         camera_base_tf = utils.matrix_to_pose(camera_base_tf_mat)
 
         # Make the camera calibration response
-        response.success = True
+        response.success.data = True
         response.camera_base_tf.transform.translation = camera_base_tf.position
         response.camera_base_tf.transform.rotation = camera_base_tf.orientation
-        response.camera_base_tf.header.frame_id = "camera_base_link"
+        response.camera_base_tf.header.frame_id = "irb120_base"
+        response.camera_base_tf.child_frame_id = "camera_color_frame"
         response.camera_base_tf.header.stamp = rospy.Time.now()
+        self.tf_broadcaster.sendTransform(response.camera_base_tf)
 
         return response
 
@@ -118,7 +153,7 @@ class CameraCalibrationService(object):
         tf_mat = np.eye(4)
         tf_mat[0:3,0:3] = r_mat
 
-        quat = tf.transformations.quaternion_from_matrix(tf_mat)
+        quat = transformations.quaternion_from_matrix(tf_mat)
 
         marker_pose.position.x = tvec[0]
         marker_pose.position.y = tvec[1]
